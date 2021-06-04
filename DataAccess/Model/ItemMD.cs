7780167a4f2c57e9762e315429197ecdac3d4f52
UrusaSGeelani <85352588+UrusaSGeelani@users.noreply.github.com>
2021-06-04 19:51:48 +0530

    Add files via upload

---
+++ DataAccess/Model/ItemMD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Model
+{
+    public class ItemMD
+    {
+        public System.Guid ItemId { get; set; }
+        [Display(Name = "Name")]
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<System.Guid> CreatedBy { get; set; }
+        public Nullable<bool> Status { get; set; }
+    }
+}
